b14f28d4e23c0b00c868a102acae32db2e1d9ff5
J.Kiboi <101619259+Muthonikiboi@users.noreply.github.com>
2023-02-09 01:13:30 +0300

    Create intro.array.js

---
+++ intro.array.js
@@ -0,0 +1,22 @@
+//literal
+let intro=["LOVE","JOY","PEACE","PATIENCE","KINDNESS","GOODNESS","FAITHFULNESS"];
+    for(i=0;i<intro.length;i++){
+        console.log(intro[i]);
+    }
+
+//usimg new keyword
+var i;
+let arr=new Array();
+    arr[0]="Jane";
+    arr[1]="Carol";
+    arr[2]="Lilian";
+
+    for(i=0;i<arr.length;i++){
+        console.log(arr[i]);
+    }
+
+//using contractors
+let varies=new Array("LOVE","JOY","PEACE","PATIENCE","KINDNESS","GOODNESS","FAITHFULNESS");
+for(s=0;s<varies.length;s++){
+    console.log(varies[s])
+}
